4113dc400905e961650d156060456bcf313914f1
sehnoh <sehnoh@4631a5be-f96c-4e43-8409-b1b998638391>
2010-06-21 07:33:22 +0000

    Added the main service, xmpp manager.

--- androidpn-client/trunk/src/org/androidpn/sdk/MainService.java
+++ androidpn-client/trunk/src/org/androidpn/sdk/MainService.java
@@ -15,35 +15,152 @@
  */
 package org.androidpn.sdk;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
+import java.util.Random;
 
-/** 
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.PacketListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.filter.AndFilter;
+import org.jivesoftware.smack.filter.PacketFilter;
+import org.jivesoftware.smack.filter.PacketTypeFilter;
+import org.jivesoftware.smack.packet.Packet;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.wifi.WifiManager;
+import android.os.IBinder;
+import android.os.Message;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+/**
  * Class desciption here.
- *
+ * 
  * @author Sehwan Noh (sehnoh@gmail.com)
  */
 public class MainService extends Service {
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
+	public static final String SERVICE_NAME = "org.androidpn.sdk.MainService";
 
-    @Override
-    public void onCreate() {
-        //
-    }
+	private static final String LOGTAG = "MainService";
 
-    @Override
-    public void onStart(Intent intent, int startId) {
-        //
-    }
+	private SharedPreferences sdkPreferences;
 
-    @Override
-    public void onDestroy() {
-        //
-    }
+	private TelephonyManager telephonyManager;
+
+	private WifiManager wifiManager;
+
+	private ConnectivityManager connectivityManager;
+
+	private String deviceId;
+
+	public MainService() {
+	}
+
+	@Override
+	public void onCreate() {
+		Log.d(getClass().getSimpleName(), "onCreate()...");
+
+		sdkPreferences = getSharedPreferences(ServiceManager.SDK_PREFERENCES,
+				Context.MODE_PRIVATE);
+
+		telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+		wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+		connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+		deviceId = telephonyManager.getDeviceId();
+		Log.d(getClass().getSimpleName(), "deviceId=" + deviceId);
+
+		if (deviceId == null || deviceId.trim().length() == 0
+				|| deviceId.matches("0+")) {
+			if (sdkPreferences.contains("EMULATOR_DEVICE_ID")) {
+				deviceId = sdkPreferences.getString("EMULATOR_DEVICE_ID", "");
+			} else {
+				deviceId = (new StringBuilder("EMU")).append(
+						(new Random(System.currentTimeMillis())).nextLong())
+						.toString();
+				sdkPreferences.edit().putString("EMULATOR_DEVICE_ID", deviceId);
+				sdkPreferences.edit().commit();
+			}
+		}
+
+//		try {
+//			ConnectionConfiguration connConfig = new ConnectionConfiguration(
+//					"192.168.123.101", 5222, "vysper");
+//			// connConfig.setSecurityMode(SecurityMode.disabled);
+//
+//			XMPPConnection connection = new XMPPConnection(connConfig);
+//			connection.connect();
+//			Log.e(LOGTAG, "XMPP CONNECTED......");
+//
+//			PacketFilter packetFilter = new AndFilter(new PacketTypeFilter(
+//					Message.class));
+//			;
+//
+//			PacketListener packetListener = new PacketListener() {
+//
+//				public void processPacket(Packet packet) {
+//
+//					Log.e(LOGTAG, "packet.toString()=" + packet.toString());
+//					Log.e(LOGTAG, "packet.toXML()=" + packet.toXML());
+//
+//				}
+//
+//			};
+//
+//			connection.addPacketListener(packetListener, packetFilter);
+//
+//			// Login Information
+//			connection.login("user1@vysper.org", "password1");
+//			// connection.login("scott", "tiger");
+//			Log.e(LOGTAG, "XMPP LOGGED IN......");
+//
+//		} catch (XMPPException xe) {
+//			xe.printStackTrace();
+//		}
+		
+		XmppManager xmppManager = new XmppManager(this);
+		xmppManager.connect();
+		//xmppManager.login();
+
+	}
+
+	@Override
+	public void onStart(Intent intent, int startId) {
+		Log.d(getClass().getSimpleName(), "onStart()...");
+
+	}
+
+	@Override
+	public void onDestroy() {
+		Log.d(getClass().getSimpleName(), "onDestroy()...");
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		Log.d(getClass().getSimpleName(), "onBind()...");
+		return null;
+	}
+
+	@Override
+	public void onRebind(Intent intent) {
+		Log.d(getClass().getSimpleName(), "onRebind()...");
+	}
+
+	@Override
+	public boolean onUnbind(Intent intent) {
+		Log.d(getClass().getSimpleName(), "onUnbind()...");
+		return true;
+	}
+
+	// ================
+
+	public static Intent getIntent() {
+		return new Intent(SERVICE_NAME);
+	}
 
 }
--- androidpn-client/trunk/src/org/androidpn/sdk/XmppManager.java
+++ androidpn-client/trunk/src/org/androidpn/sdk/XmppManager.java
@@ -15,11 +15,119 @@
  */
 package org.androidpn.sdk;
 
-/** 
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+/**
  * Class desciption here.
- *
+ * 
  * @author Sehwan Noh (sehnoh@gmail.com)
  */
 public class XmppManager {
+
+	private static final String LOGTAG = "XmppManager";
+
+	private Context context;
+
+	private SharedPreferences sdkPreferences;
+
+	private String xmppHost;
+
+	private int xmppPort;
+
+	private XMPPConnection connection;
+
+	private String username;
+
+	private String password;
+
+	private boolean running;
+
+	public XmppManager(Context context) {
+		this.context = context;
+		this.sdkPreferences = context.getSharedPreferences(
+				ServiceManager.SDK_PREFERENCES, Context.MODE_PRIVATE);
+
+		this.xmppHost = sdkPreferences.getString(KeyConstants.XMPP_HOST,
+				"localhost");
+		this.xmppPort = sdkPreferences.getInt(KeyConstants.XMPP_PORT, 5222);
+		this.username = sdkPreferences.getString(KeyConstants.XMPP_USERNAME,
+				username);
+		this.password = sdkPreferences.getString(KeyConstants.XMPP_PASSWORD,
+				password);
+	}
+
+	private boolean isConnected() {
+		return connection != null && connection.isConnected();
+	}
+
+	private boolean isAuthenticated() {
+		return connection != null && connection.isConnected()
+				&& connection.isAuthenticated();
+	}
+
+	private boolean isRegistered() {
+		return sdkPreferences.contains(KeyConstants.XMPP_USERNAME)
+				&& sdkPreferences.contains(KeyConstants.XMPP_PASSWORD);
+	}
+
+	public void connect() {
+		Log.e(LOGTAG, "connect().....");
+		if (!isConnected()) {
+			// Create the configuration for this new connection
+			ConnectionConfiguration config = new ConnectionConfiguration(
+					xmppHost, xmppPort);
+			config.setCompressionEnabled(true);
+			config.setSASLAuthenticationEnabled(true);
+
+			connection = new XMPPConnection(config);
+
+			try {
+				// Connect to the server
+				connection.connect();
+
+				// ProviderManager.getInstance()
+				// .addIQProvider("registration",
+				// "xtify:iq:registration",
+				// new RegistrationProvider());
+
+			} catch (XMPPException xe) {
+				Log.e(LOGTAG, "connect()..... failed.", xe);
+			}
+		}
+	}
+
+	public void login() {
+		Log.e(LOGTAG, "login().....");
+		if (!isAuthenticated()) {
+			try {
+				connection.login(username, password, "AndroidpnClient");
+				Log.i(LOGTAG, "login()..... logged in successfully.");
+
+			} catch (XMPPException xe) {
+				Log.e(LOGTAG, "login()..... failed by a typical error.", xe);
+
+			} catch (Exception e) {
+				Log.e(LOGTAG, "login()..... failed by an unexpected error.", e);
+			}
+		} else {
+			Log.i(LOGTAG, "login()..... logged in already.");
+		}
+	}
+
+	public void register() {
+		Log.e(LOGTAG, "register().....");
+
+		if (!isRegistered()) {
+
+		} else {
+
+		}
+	}
 
 }
