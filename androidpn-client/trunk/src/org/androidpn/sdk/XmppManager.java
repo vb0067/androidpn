cdccf6ff048acc279f71ad0ac7b8cdcd3a113b25
sehnoh <sehnoh@4631a5be-f96c-4e43-8409-b1b998638391>
2010-06-22 01:16:30 +0000

    Modified XmppManager.

--- androidpn-client/trunk/src/org/androidpn/sdk/XmppManager.java
+++ androidpn-client/trunk/src/org/androidpn/sdk/XmppManager.java
@@ -16,11 +16,16 @@
 package org.androidpn.sdk;
 
 import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.PacketListener;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.IQ;
+import org.jivesoftware.smack.packet.Packet;
+import org.jivesoftware.smack.packet.Registration;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.util.Log;
 
 /**
@@ -30,104 +35,221 @@ import android.util.Log;
  */
 public class XmppManager {
 
-	private static final String LOGTAG = "XmppManager";
+    private static final String LOGTAG = "XmppManager";
 
-	private Context context;
+    // private Context context;
 
-	private SharedPreferences sdkPreferences;
+    private SharedPreferences sdkPreferences;
 
-	private String xmppHost;
+    private Editor editor;
 
-	private int xmppPort;
+    private String xmppHost;
 
-	private XMPPConnection connection;
+    private int xmppPort;
 
-	private String username;
+    private XMPPConnection connection;
 
-	private String password;
+    private String username;
 
-	private boolean running;
+    private String password;
 
-	public XmppManager(Context context) {
-		this.context = context;
-		this.sdkPreferences = context.getSharedPreferences(
-				ServiceManager.SDK_PREFERENCES, Context.MODE_PRIVATE);
+    // private boolean running;
 
-		this.xmppHost = sdkPreferences.getString(KeyConstants.XMPP_HOST,
-				"localhost");
-		this.xmppPort = sdkPreferences.getInt(KeyConstants.XMPP_PORT, 5222);
-		this.username = sdkPreferences.getString(KeyConstants.XMPP_USERNAME,
-				username);
-		this.password = sdkPreferences.getString(KeyConstants.XMPP_PASSWORD,
-				password);
-	}
+    public XmppManager(Context context) {
+        // this.context = context;
+        this.sdkPreferences = context.getSharedPreferences(
+                ServiceManager.SDK_PREFERENCES, Context.MODE_PRIVATE);
+        this.editor = sdkPreferences.edit();
 
-	private boolean isConnected() {
-		return connection != null && connection.isConnected();
-	}
+        this.xmppHost = sdkPreferences.getString(KeyConstants.XMPP_HOST,
+                "localhost");
+        this.xmppPort = sdkPreferences.getInt(KeyConstants.XMPP_PORT, 5222);
+        this.username = sdkPreferences.getString(KeyConstants.XMPP_USERNAME,
+                username);
+        this.password = sdkPreferences.getString(KeyConstants.XMPP_PASSWORD,
+                password);
+    }
 
-	private boolean isAuthenticated() {
-		return connection != null && connection.isConnected()
-				&& connection.isAuthenticated();
-	}
+    private boolean isConnected() {
+        return connection != null && connection.isConnected();
+    }
 
-	private boolean isRegistered() {
-		return sdkPreferences.contains(KeyConstants.XMPP_USERNAME)
-				&& sdkPreferences.contains(KeyConstants.XMPP_PASSWORD);
-	}
+    private boolean isAuthenticated() {
+        return connection != null && connection.isConnected()
+                && connection.isAuthenticated();
+    }
 
-	public void connect() {
-		Log.e(LOGTAG, "connect().....");
-		if (!isConnected()) {
-			// Create the configuration for this new connection
-			ConnectionConfiguration config = new ConnectionConfiguration(
-					xmppHost, xmppPort);
-			config.setCompressionEnabled(true);
-			config.setSASLAuthenticationEnabled(true);
+    private boolean isRegistered() {
+        return sdkPreferences.contains(KeyConstants.XMPP_USERNAME)
+                && sdkPreferences.contains(KeyConstants.XMPP_PASSWORD);
+    }
 
-			connection = new XMPPConnection(config);
+    private void removeRegistration() {
+        editor.remove(KeyConstants.XMPP_USERNAME);
+        editor.remove(KeyConstants.XMPP_PASSWORD);
+        editor.commit();
+    }
 
-			try {
-				// Connect to the server
-				connection.connect();
+    //	public void connectToXmpp() {
+    //		connect();
+    //		login();
+    //		//register();
+    //	}
 
-				// ProviderManager.getInstance()
-				// .addIQProvider("registration",
-				// "xtify:iq:registration",
-				// new RegistrationProvider());
+    public void connect() {
+        Log.d(LOGTAG, "connect().....");
+        if (!isConnected()) {
+            // Create the configuration for this new connection
+            ConnectionConfiguration config = new ConnectionConfiguration(
+                    xmppHost, xmppPort, "AndroidpnClient");
+            //            config.setCompressionEnabled(true);
+            //            config.setSASLAuthenticationEnabled(true);
 
-			} catch (XMPPException xe) {
-				Log.e(LOGTAG, "connect()..... failed.", xe);
-			}
-		}
-	}
+            connection = new XMPPConnection(config);
 
-	public void login() {
-		Log.e(LOGTAG, "login().....");
-		if (!isAuthenticated()) {
-			try {
-				connection.login(username, password, "AndroidpnClient");
-				Log.i(LOGTAG, "login()..... logged in successfully.");
+            try {
+                // Connect to the server
+                connection.connect();
 
-			} catch (XMPPException xe) {
-				Log.e(LOGTAG, "login()..... failed by a typical error.", xe);
+                // ProviderManager.getInstance()
+                // .addIQProvider("registration",
+                // "xtify:iq:registration",
+                // new RegistrationProvider());
 
-			} catch (Exception e) {
-				Log.e(LOGTAG, "login()..... failed by an unexpected error.", e);
-			}
-		} else {
-			Log.i(LOGTAG, "login()..... logged in already.");
-		}
-	}
+            } catch (XMPPException xe) {
+                Log.e(LOGTAG, "connect()..... failed.", xe);
+            }
+        }
+        Log.d(LOGTAG, "connect()..... done!");
+    }
 
-	public void register() {
-		Log.e(LOGTAG, "register().....");
+    public void login() {
+        Log.d(LOGTAG, "login().....");
 
-		if (!isRegistered()) {
+        //        Log.e(LOGTAG, "isConnected()=" + connection.isConnected());
+        //        Log.e(LOGTAG, "isAuthenticated()=" + connection.isAuthenticated());
+        //        Log.e(LOGTAG, "isRegistered()=" + isRegistered());
+        //        Log.e(LOGTAG, "!isAuthenticated() && isRegistered()="
+        //                + (!isAuthenticated() && isRegistered()));
 
-		} else {
+        if (isRegistered()) {
+            if (!isAuthenticated()) {
+                try {
+                    Log.d(LOGTAG, "username=" + username);
+                    Log.d(LOGTAG, "password=" + password);
 
-		}
-	}
+                    //connection.login(username, password, "AndroidpnClient");
+                    connection.login(username, password);
+                    Log.i(LOGTAG, "login()..... logged in successfully.");
+
+                } catch (XMPPException xe) {
+                    Log
+                            .e(LOGTAG,
+                                    "login()..... failed by a typical error.",
+                                    xe);
+                    removeRegistration();
+                    register();
+                    login();
+
+                } catch (Exception e) {
+                    Log.e(LOGTAG,
+                            "login()..... failed by an unexpected error.", e);
+                }
+            } else {
+                Log.i(LOGTAG, "login()..... logged in already.");
+            }
+        } else {
+            Log.i(LOGTAG, "login()..... not yet registered.");
+        }
+        Log.d(LOGTAG, "login()..... done!");
+    }
+
+    public void register() {
+        Log.d(LOGTAG, "register().....");
+
+        if (!isRegistered()) {
+
+            // final XmppManager xmppManager = XmppManager.this;
+            final String rUsername = UUIDGenerator.newRandomUUID();
+            final String rPassword = UUIDGenerator.newRandomUUID();
+
+            Registration registration = new Registration();
+            //            PacketFilter packetFilter = new AndFilter(new PacketIDFilter(
+            //                    registration.getPacketID()), new PacketTypeFilter(
+            //                    Message.class));
+            //            ;
+
+            PacketListener packetListener = new PacketListener() {
+
+                public void processPacket(Packet packet) {
+
+                    Log.d("PacketListener", "processPacket().....");
+                    Log.e("PacketListener", "packet=" + packet.toXML());
+
+                    if (packet instanceof IQ) {
+                        IQ response = (IQ) packet;
+                        if (response.getType() == IQ.Type.ERROR) {
+                            if (!response.getError().toString().contains("409")) {
+                                Log.e(LOGTAG,
+                                        "Unknown error while registering XMPP account! "
+                                                + response.getError()
+                                                        .getCondition());
+                            }
+                        } else if (response.getType() == IQ.Type.RESULT) {
+                            username = rUsername;
+                            password = rPassword;
+                            Log.d(LOGTAG, "username=" + username);
+                            Log.d(LOGTAG, "password=" + password);
+
+                            editor.putString(KeyConstants.XMPP_USERNAME,
+                                    username);
+                            editor.putString(KeyConstants.XMPP_PASSWORD,
+                                    password);
+                            editor.commit();
+
+                            Log.i(LOGTAG, "XMPP account registered.");
+                            //login();
+                        }
+                    }
+                }
+            };
+
+            //connection.addPacketListener(packetListener, packetFilter);
+            connection.addPacketListener(packetListener, null);
+            registration.setType(IQ.Type.SET);
+            //registration.setTo(xmppHost);
+            //            Map<String, String> attributes = new HashMap<String, String>();
+            //            attributes.put("username", rUsername);
+            //            attributes.put("password", rPassword);
+            //            registration.setAttributes(attributes);
+            registration.addAttribute("username", rUsername);
+            registration.addAttribute("password", rPassword);
+            connection.sendPacket(registration);
+
+        } else {
+            Log.i(LOGTAG, "register()..... account registered already.");
+        }
+        Log.d(LOGTAG, "register()..... done!");
+    }
+
+    //    public static String getUsername(XmppManager xmppManager) {
+    //        return xmppManager.username;
+    //    }
+    //
+    //    public static String getPassword(XmppManager xmppManager) {
+    //        return xmppManager.password;
+    //    }
+    //
+    //    public static void setUsername(XmppManager xmppManager, String username) {
+    //        xmppManager.username = username;
+    //    }
+    //
+    //    public static void setPassword(XmppManager xmppManager, String password) {
+    //        xmppManager.password = password;
+    //    }
+    //
+    //    public static SharedPreferences getSdkPreferences(XmppManager xmppManager) {
+    //        return xmppManager.sdkPreferences;
+    //    }
 
 }
