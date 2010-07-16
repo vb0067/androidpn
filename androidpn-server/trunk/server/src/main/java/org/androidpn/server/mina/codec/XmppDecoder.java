8344356b6ac456c78d55ca9ec340171388864ba2
sehnoh <sehnoh@4631a5be-f96c-4e43-8409-b1b998638391>
2010-07-16 06:20:48 +0000

    Added MINA XmppCodecFactory

---
+++ androidpn-server/trunk/server/src/main/java/org/androidpn/server/mina/codec/XmppDecoder.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.androidpn.server.mina.codec;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.mina.core.buffer.IoBuffer;
+import org.apache.mina.core.session.IoSession;
+import org.apache.mina.filter.codec.CumulativeProtocolDecoder;
+import org.apache.mina.filter.codec.ProtocolDecoderOutput;
+import org.jivesoftware.openfire.nio.XMLLightweightParser;
+
+/** 
+ * Class desciption here.
+ *
+ * @author Sehwan Noh (sehnoh@gmail.com)
+ */
+public class XmppDecoder extends CumulativeProtocolDecoder {
+
+    private final Log log = LogFactory.getLog(XmppDecoder.class);
+
+    @Override
+    public boolean doDecode(IoSession session, IoBuffer in,
+            ProtocolDecoderOutput out) throws Exception {
+        log.debug("doDecode(...)...");
+
+        // Get the XML light parser from the IoSession
+        XMLLightweightParser parser = (XMLLightweightParser) session
+                .getAttribute("XML-PARSER");
+        // Parse as many stanzas as possible from the received data
+        parser.read(in);
+
+        if (parser.areThereMsgs()) {
+            for (String stanza : parser.getMsgs()) {
+                out.write(stanza);
+            }
+        }
+        return !in.hasRemaining();
+    }
+
+}
