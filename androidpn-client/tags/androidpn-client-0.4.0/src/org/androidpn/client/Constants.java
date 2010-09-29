afe33a4ce610f64ef9ca03e79255509c155cb3b7
sehnoh <sehnoh@4631a5be-f96c-4e43-8409-b1b998638391>
2010-09-29 06:00:45 +0000

    androidpn client version 0.4.0

---
+++ androidpn-client/tags/androidpn-client-0.4.0/src/org/androidpn/client/Constants.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2010 The Androidpn Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.androidpn.client;
+
+/**
+ * Static constants for this package.
+ * 
+ * @author Sehwan Noh (sehnoh@gmail.com)
+ */
+public class Constants {
+
+    public static final String CLIENT_VERSION = "0.4.0";
+
+    public static final String CLIENT_PREFERENCES = "ClientPreferences";
+
+    // PREFERENCE KEYS
+
+    public static final String ANDROIDPN_API_KEY = "ANDROIDPN_API_KEY";
+
+    public static final String XMPP_HOST = "XMPP_HOST";
+
+    public static final String XMPP_PORT = "XMPP_PORT";
+
+    public static final String XMPP_USERNAME = "XMPP_USERNAME";
+
+    public static final String XMPP_PASSWORD = "XMPP_PASSWORD";
+
+    public static final String USER_KEY = "USER_KEY";
+
+    public static final String DEVICE_ID = "DEVICE_ID";
+
+    public static final String EMULATOR_DEVICE_ID = "EMULATOR_DEVICE_ID";
+
+    public static final String NOTIFICATION_ICON = "NOTIFICATION_ICON";
+
+    public static final String NOTIFICATION_SOUND = "NOTIFICATION_SOUND";
+
+    // NOTIFICATION FIELDS
+
+    public static final String NOTIFICATION_ID = "NOTIFICATION_ID";
+
+    public static final String NOTIFICATION_API_KEY = "NOTIFICATION_API_KEY";
+
+    public static final String NOTIFICATION_TITLE = "NOTIFICATION_TITLE";
+
+    public static final String NOTIFICATION_MESSAGE = "NOTIFICATION_MESSAGE";
+
+    public static final String NOTIFICATION_TICKER = "NOTIFICATION_TICKER";
+
+    public static final String NOTIFICATION_URL = "NOTIFICATION_URL";
+
+    // INTENT ACTIONS
+
+    public static final String ACTION_SHOW_NOTIFICATION = "org.androidpn.client.SHOW_NOTIFICATION";
+
+    public static final String ACTION_NOTIFICATION_CLICKED = "org.androidpn.client.NOTIFICATION_CLICKED";
+
+    public static final String ACTION_NOTIFICATION_CLEARED = "org.androidpn.client.NOTIFICATION_CLEARED";
+
+}
